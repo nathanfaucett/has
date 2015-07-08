64ad849348e98f5d7b2b21900e5c753923b8cb3a
nathanfaucett <nathanfaucett@gmail.com>
2015-07-08 07:38:40 -0500

    updates

---
+++ test/index.js
@@ -0,0 +1,26 @@
+var tape = require("tape"),
+    create = require("create"),
+    has = require("../src/index");
+
+
+tape("has(object, key) should return true if object has own property", function(assert) {
+    var object = {
+            key: "key"
+        },
+        protoObject = create({
+            protoKey: "protoKey"
+        }),
+        noProtoObject = create(null);
+
+    protoObject.key = "key";
+    noProtoObject.key = "key";
+
+    assert.equal(has(object, "key"), true);
+    assert.equal(has(object, "value"), false);
+    assert.equal(has(protoObject, "key"), true);
+    assert.equal(has(protoObject, "protoKey"), false);
+    assert.equal(has(noProtoObject, "key"), true);
+    assert.equal(has(noProtoObject, "value"), false);
+
+    assert.end();
+});
